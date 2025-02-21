b4b0800cfc9c60b55e37617167a5420f22ff7f72
Kevin Siegall <digimonlord2@gmail.com>
2025-02-20 21:55:58 -0500

    disabled broken contact form

--- components/contact-popup.tsx
+++ components/contact-popup.tsx
@@ -8,10 +8,10 @@ export default function ContactPopup(props: any) {
     return <>
         <div className={"flex flex-col gap-2 text-left bg-zinc-900 fixed transition max-h-screen max-w-[600px] z-40 bottom-40 w-8/12 h-10/12 z-10 p-5 text-xs text-center drop-shadow-lg shadow-white-200 right-10 origin-right" + (open ? " opacity-100" : " -rotate-90 opacity-0 translate-x-20")}>
             <button className="ml-auto" onClick={() => setOpen(!open)}><FaTimesCircle /></button>
-            <h5 className="text-xl">Kay Siegall</h5>
+            <h5 className="text-xl">Kevin Siegall</h5>
             <p className="text-left text-sm">Question? Work inquiry? Just want to chat? I'd love to hear from you!</p>
-            <p className="text-left text-xs">To protect my inbox from evil robots, please find my contact info on my PDF resume (linked below), or fill out this form 🤖</p>
-            <iframe style={{minHeight: 160}} src="/contact.html">Failed to load contact form</iframe>
+            <p className="text-left text-xs">To protect my inbox from bots, please find my contact info on my PDF resume (linked below)</p>
+            {/* <iframe style={{minHeight: 160}} src="/fail">Failed to load contact form</iframe> */}
             {/* <input type="text" className="p-1 rounded-md" placeholder="Your Email/Callback #" />
             <textarea className="p-1 rounded-md h-20 resize-none" placeholder="Message" /> */}
             <div className="flex flex-row align-center justify-center gap-6 sm:gap-12 text-2xl mt-4">
